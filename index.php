04c51c93ccdc5c7f7884e9ff09e915d53508302c
Pajkanaon <Jocce@jocce.nu>
2014-10-08 19:44:52 +0200

    Moved CSS

--- index.php
+++ index.php
@@ -1,56 +1,14 @@
 <html>
 <head>
 <title>Controll Panel ala JoccE</title>
-<style>
-body {
-    background-color: black;
-    color: white;
-}
-#header {
-	width: 1000px;	
-	height: 40px;
-	border-style: solid;
-    border-width: 2px;		
-    margin-left: auto;
-    margin-right: auto;
-    
-}
-#body {
-	width: 1000px;	
-	border-style: solid;
-    border-width: 2px;	
-    margin-left: auto;
-    margin-right: auto;
-    
-}
-#footer {
-	height: 20px;
-	width: 1000px;	
-	border-style: solid;
-    border-width: 2px;	
-    margin-left: auto;
-    margin-right: auto;
-}
-.left {
-	float: left;
-	margin-top: 6px;
-	margin-left: 4px;
-}
-.right{
-	float: left;
-	margin-top: 6px;
-}
-#console{
-	margin-top: -40px;	
-}
-</style>
+<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 </head>
 <body>
 <?php
 	
 	//Multicraft API Settings
 	require('MulticraftAPI.php');
-	$api = new MulticraftAPI('http://example.com/api.php', 'YOURUSER', 'YOURAPICODE');
+	$api = new MulticraftAPI('http://jocce.nu/multicraft/api.php', 'jocce', '672dfe87834abb8408e0');
 	
 	//ServerID here
 	$serverid = 9;
@@ -62,10 +20,8 @@ body {
 	$servercheck = $api->getServerStatus($serverid, true);
 	$serverstatus = $api->getServerStatus($serverid, true)[data];
 	$sista = sizeof($api->getServerLog($serverid)[data]);	
-	//print_r($serverstatus[status]);
+	//print_r($serverstatus[status]);	
 ?>	
-	
-	
 <div id="header">
 	<form method="post"  action="start.php" class="left">
 	<input type="submit" value="Start Server" name="Start Server" /></form>
@@ -80,16 +36,16 @@ body {
 
 
 <div id="body">
-		<h2>Console:</h2>
-		<div id="console">
-		<?php
-		for ($i = $sista; $i >= $sista-$rows; $i--) {
-		
-		print_r($api->getServerLog(9)[data][$i]['line']);
-		print "<br/>";
-		}
-		?>
-		</div>
+	<h2>Console:</h2>
+	<div id="console">
+	<?php
+	for ($i = $sista; $i >= $sista-$rows; $i--) {
+	
+	print_r($api->getServerLog(9)[data][$i]['line']);
+	print "<br/>";
+	}
+	?>
+	</div>		
 </div>
 <div id="footer">Restart Script made by JoccE</div>
 
