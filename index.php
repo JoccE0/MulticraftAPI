726f8dbd1c0ea44ca495f3d21bc4add9253fe239
Pajkanaon <Jocce@jocce.nu>
2014-10-08 16:47:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-	//Multicraft API Settings
-	require('MulticraftAPI.php');
-	$api = new MulticraftAPI('http://jocce.nu/multicraft/api.php', 'jocce', '672dfe87834abb8408e0');
-	
-	//ServerID here
-	$serverid = 9;
-	
-	//Seconds between restart
-	$restarttime = 5;
-	
-	//dont touch
-	$servercheck = $api->getServerStatus($serverid, true);
-	$serverstatus = $api->getServerStatus($serverid, true)[data];
-	$sista = sizeof($api->getServerLog($serverid)[data]);	
-	//print_r($serverstatus[status]);
-?>	
-
 <html>
 <head>
 <title>Controll Panel ala JoccE</title>
@@ -31,6 +13,7 @@ body {
     border-width: 2px;		
     margin-left: auto;
     margin-right: auto;
+    
 }
 #body {
 	width: 1000px;	
@@ -38,7 +21,7 @@ body {
     border-width: 2px;	
     margin-left: auto;
     margin-right: auto;
-    min-height: 100px;
+    
 }
 #footer {
 	height: 20px;
@@ -53,21 +36,61 @@ body {
 	margin-top: 6px;
 	margin-left: 4px;
 }
+.right{
+	float: left;
+	margin-top: 6px;
+}
+#console{
+	margin-top: -40px;	
+}
 </style>
 </head>
 <body>
+<?php
+	
+	//Multicraft API Settings
+	require('MulticraftAPI.php');
+	$api = new MulticraftAPI('http://example.com/api.php', 'YOURUSER', 'YOURAPICODE');
+	
+	//ServerID here
+	$serverid = 9;
+	
+	//how many rows to show
+	$rows = 50;	
+	
+	//dont touch. variables for stuff
+	$servercheck = $api->getServerStatus($serverid, true);
+	$serverstatus = $api->getServerStatus($serverid, true)[data];
+	$sista = sizeof($api->getServerLog($serverid)[data]);	
+	//print_r($serverstatus[status]);
+?>	
+	
+	
 <div id="header">
-	<form method="post"  action="index.php" class="left">
-	<input type="submit" value="Back" name="Back" /></form>
-</div>
-<div id="body">
-	<?php	
-	sleep($restarttime);
-	$api->restartServer($serverid);
-	print ("Server is restarting in!");
-	?>	
+	<form method="post"  action="start.php" class="left">
+	<input type="submit" value="Start Server" name="Start Server" /></form>
+	
+	<form method="post"  action="restart.php" class="right">
+	<input type="submit" value="Restart Server" name="Restart Server" /></form>
+	
+	<a href="http://crash.ftbees.com/" class="right">
+    <button>Crash Logs</button>
+	</a>
 </div>
 
+
+<div id="body">
+		<h2>Console:</h2>
+		<div id="console">
+		<?php
+		for ($i = $sista; $i >= $sista-$rows; $i--) {
+		
+		print_r($api->getServerLog(9)[data][$i]['line']);
+		print "<br/>";
+		}
+		?>
+		</div>
+</div>
 <div id="footer">Restart Script made by JoccE</div>
 
 </body>
