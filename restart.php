c0a246c5ca9b051dc35106909202cf33d9c75769
Pajkanon <Jocce@jocce.nu>
2014-10-24 02:44:58 +0200

    Update restart.php

--- restart.php
+++ restart.php
@@ -1,40 +1,17 @@
 <?php
 	//Multicraft API Settings
 	require('MulticraftAPI.php');
-	$api = new MulticraftAPI('http://example.com/api.php', 'YOURUSER', 'YOURAPICODE');
+	$api = new MulticraftAPI('http://jocce.nu/multicraft/api.php', 'jocce', '672dfe87834abb8408e0');
 	
 	//ServerID here
-	$serverid = 9;
-	
-	//Seconds between restart
-	$restarttime = 5;
-	
+	$serverid = 10;
+		
 	//dont touch
 	$servercheck = $api->getServerStatus($serverid, true);
 	$serverstatus = $api->getServerStatus($serverid, true)[data];
 	$sista = sizeof($api->getServerLog($serverid)[data]);	
 	//print_r($serverstatus[status]);
-?>	
 
-<html>
-<head>
-<title>Controll Panel ala JoccE</title>
-<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-</head>
-<body>
-<div id="header">
-	<form method="post"  action="index.php" class="left">
-	<input type="submit" value="Back" name="Back" /></form>
-</div>
-<div id="body">
-	<?php	
-	sleep($restarttime);
 	$api->restartServer($serverid);
-	print ("Server is restarting in!");
-	?>	
-</div>
-
-<div id="footer">Restart Script made by JoccE</div>
-
-</body>
-</html>
+	print ("Server is restarting!");
+?>	
