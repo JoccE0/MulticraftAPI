4fb31b586ac4edc36519a5072b6690b3fc7ab74d
Pajkanon <Jocce@jocce.nu>
2014-10-24 02:45:14 +0200

    Update start.php

--- start.php
+++ start.php
@@ -1,10 +1,10 @@
 <?php
 	//Multicraft API Settings
 	require('MulticraftAPI.php');
-	$api = new MulticraftAPI('http://example.com/api.php', 'YOURUSER', 'YOURAPICODE');
+	$api = new MulticraftAPI('http://jocce.nu/multicraft/api.php', 'jocce', '672dfe87834abb8408e0');
 	
 	//ServerID here
-	$serverid = 9;
+	$serverid = 10;
 	
 	//how many rows to show
 	$rows = 50;	
@@ -14,20 +14,8 @@
 	$serverstatus = $api->getServerStatus($serverid, true)[data];
 	$sista = sizeof($api->getServerLog($serverid)[data]);	
 	//print_r($serverstatus[status]);
-?>	
 
-<html>
-<head>
-<title>Controll Panel ala JoccE</title>
-<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-</head>
-<body>
-<div id="header">
-	<form method="post"  action="index.php" class="left">
-	<input type="submit" value="Back" name="Back" /></form>
-</div>
-<div id="body">
-	<?php
+
 	//checks if server is started - Starts if offline otherwise nothing
 	if ($serverstatus[status] == online) {
 	
@@ -41,9 +29,3 @@
 	}
 	
 	?>	
-
-</div>
-<div id="footer">Restart Script made by JoccE</div>
-
-</body>
-</html>
